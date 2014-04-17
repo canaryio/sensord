aeddf16553f23e055ff4d1b0d512d9f72c93acac
Michael Gorsuch <michael.gorsuch@gmail.com>
2014-04-17 00:29:38 +0000

    we are correctly parsing curly data now

--- hello.go
+++ hello.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os/exec"
 )
 
-func main() {
-	curlyCmd := exec.Command("curly", "https://github.com")
+// {"url": "http://github.com", "connect_time": 0.031178999999999998, "exit_status": 0, "starttransfer_time": 0.031178999999999998, "t": 1397688864, "local_ip": "107.170.123.131", "primary_ip": "192.30.252.128", "total_time": 0.037648000000000001, "http_status": 301, "namelookup_time": 0.024646000000000001, "local_port": 53858}
+type curly_response struct {
+	Url               string  `json:"url"`
+	ConnectTime       float64 `json:"connect_time"`
+	ExitStatus        int     `json:"exit_status"`
+	StartTransferTime float64 `json:"starttransfer_time"`
+	T                 int     `json:"t"`
+	LocalIp           string  `json:"local_ip"`
+	PrimaryIp         string  `json:"primary_ip"`
+	TotalTime         float64 `json:"total_time"`
+	HttpStatus        int     `json:"http_status"`
+	NameLookupTime    float64 `json:"namelookup_time"`
+}
 
-	cmdOut, err := curlyCmd.Output()
-	if err != nil {
-		panic(err)
+func main() {
+	cmd := exec.Command("curly", "http://github.com")
+	cmdOut, _ := cmd.Output()
+
+	var foo curly_response
+	if err := json.Unmarshal(cmdOut, &foo); err != nil {
+		log.Fatalf("error %v", err)
 	}
-	fmt.Print(string(cmdOut))
+	fmt.Printf("%s\n", cmdOut)
+	fmt.Printf("%v\n", foo)
 }
